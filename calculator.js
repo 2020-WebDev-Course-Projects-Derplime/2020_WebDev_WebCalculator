0a779952ec3f9289ef1c517d4edb5b7e84457f54
Emil Dolorfino <edolorfino24@gmail.com>
2020-05-21 04:59:20 -0400

    Finished BMI calculator

--- calculator.js
+++ calculator.js
@@ -21,6 +21,20 @@ app.post("/", function (req, res) {
     res.send(`Thanks for posting that brah. The result of your addition is ${result}.`);
 });
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+
+    // Deconstruct body
+    const { weight, height } = req.body;
+
+    let result = (Number(weight) / (Number(height) * Number(height)));
+
+    res.send(`Your BMI is ${result}`);
+});
+
 app.listen(3000, function () {
     console.log(`Express server running on port 3000`);
 });
